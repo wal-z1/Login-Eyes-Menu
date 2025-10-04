f7773abd26a018209b950832744c5807997d921b
Walid <ow_bouhenika@esi.dz>
2025-10-04 10:52:37 +0100

    Login for with hiver buttons and using form fotr the div full container for more sematic look

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,9 +1,50 @@
-function LoginForm() {
+import { motion } from "framer-motion";
+
+export default function LoginForm() {
 	return (
-		<div className="bg-[#f3f3f5] h-[96%] flex justify-center m-5 ">
-			LoginForm
-		</div>
+		<motion.div className=" w-[98%] p-6">
+			<h2 className="text-2xl font-semibold mb-2">Welcome back!</h2>
+			<p className="text-gray-600 mb-8">Please enter your details.</p>
+			{/*A for wrap for the input elements */}
+
+			<form
+				className="flex flex-col gap-5" /*Flex on col and nice gapping lol*/
+			>
+				<div>
+					<label className="text-sm mb-2">Email</label>
+					<input className="w-full border border-gray-300 rounded-lg p-3 focus:ring-2 focus:ring-black outline-none" />
+				</div>
+
+				<div>
+					<label className=" text-sm mb-2">Password</label>
+					<input
+						type="password" /*to hideteh chars*/
+						className="w-full border border-gray-300 rounded-lg p-3 focus:ring-2 focus:ring-black outline-none"
+					/>
+				</div>
+				{/*recreation of the image in insta*/}
+				<div
+					className="flex items-center justify-between text-sm" /*flex for he row*/
+				>
+					<label className="flex items-center gap-2">
+						<input type="checkbox" />
+						Remember me
+					</label>
+					<a
+						href="#"
+						className="text-gray-500 hover:text-black" /*added a nice hover*/
+					>
+						Forgot password?
+					</a>
+				</div>
+
+				<button className="bg-black text-white py-2 rounded-lg hover:bg-gray-800 transition">
+					Log in
+				</button>
+				<button className="bg-gray-100 py-2 rounded-lg hover:bg-gray-200 transition">
+					Log in with Google
+				</button>
+			</form>
+		</motion.div>
 	);
 }
-
-export default LoginForm;
